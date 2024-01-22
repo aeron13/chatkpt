e3821b9b33291622814b3f54d1ecb517b798d837
aeron13 <33543173+aeron13@users.noreply.github.com>
2024-01-22 09:10:58 +0100

    conversation boxes style

--- resources/views/partials/conversations-list.blade.php
+++ resources/views/partials/conversations-list.blade.php
@@ -14,11 +14,16 @@
                         <li class="rounded-2xl bg-lightglass-1 dark:bg-glass-1 rounded-xl py-[10px] lg:py-[16px] pl-[20px] lg:pl-[25px] pr-[10px] lg:pr-[16px] border border-[0.5px] border-[#C9C9C9] dark:border-[#868686] lg:h-[162px] transform hover:rotate-[-1deg] transition-transform duration-500" >
                             <div class="w-full h-full flex flex-col justify-between items-end">
                                 <p class="w-full font-special text-2xl font-medium dark:text-light mb-[20px] mt-[5px] pr-[6px] lg:m-0 lg:p-0" x-text="post.title"></p>
-                                <div class="w-full flex relative gap-2">
+                                <div class="w-full flex relative gap-2 items-center">
                                     <template x-for="cat in post.categories">
-                                        <div class="w-[20px] h-[20px] lg:w-[32px] lg:h-[32px] origin-top-left rotate-[-4.45deg] rounded-[5px]  transform translate-x-[-10px]" x-bind:style="`background-color: ${cat.color}`"></div>
+                                        <div 
+                                            class="origin-top-left rotate-[-4.45deg] transform translate-x-[-10px]"
+                                            x-bind:class="cat.parent_id ? 'w-[13px] h-[13px] lg:w-[20px] lg:h-[20px] rounded-[4px]' : 'rounded-[5px] w-[20px] h-[20px] lg:w-[32px] lg:h-[32px]' " 
+                                            x-bind:style="`background-color: ${cat.color}`"
+                                        >
+                                        </div>
                                     </template>
-                                    <button @click.prevent="$dispatch('open-update-modal', post.id)" class="add-icon w-[23px] h-[23px] absolute right-0 bottom-0 transform hover:scale-[1.1] transition-transform" style="background-image: url({{ asset('icons/plus-icon.svg') }})"></button>
+                                    <button @click.prevent="$dispatch('open-update-modal', post.id)" class="add-icon w-[18px] h-[18px] lg:w-[23px] lg:h-[23px] absolute right-0 bottom-0 transform hover:scale-[1.1] transition-transform" style="background-image: url({{ asset('icons/plus-icon.svg') }})"></button>
                                 </div>
                             </div>
                         </li>
@@ -28,7 +33,7 @@
         </div>
     </template>
 
-    <x-modal name="update-conversation" :show="false">
+    <x-modal name="update-conversation" :show="false" onClose="" focusable>
         <x-form-box :title="'Add category'" class="mx-0">
             <p class="font-sans dark:text-light text-[15px] mt-[36px]">Add a category to:</p>
             <p class="font-special dark:text-light text-xl lg:text-2xl" x-text="$store.api.conversation.title"></p>
